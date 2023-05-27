b181f64ac69753a265f5697dc66635186ef17377
Mar-Issah <masy370@gmail.com>
2023-05-26 21:14:03 -0400

    revert changes

--- stack_func2.c
+++ stack_func2.c
@@ -1,4 +1,37 @@
 #include "monty.h"
+/**
+ * _add - add top of stack y second top stack
+ * @stack: pointer to lists for monty stack
+ * @number: number of line opcode occurs on
+ */
+
+void _add(stack_t **stack, unsigned int number)
+{
+	stack_t *temp = *stack;
+	int sum = 0, i = 0;
+
+	if (temp == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", number);
+		exit(EXIT_FAILURE);
+	}
+
+	while (temp)
+	{
+		temp = temp->next;
+		i++;
+	}
+
+	if (stack == NULL || (*stack)->next == NULL || i <= 1)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", number);
+		exit(EXIT_FAILURE);
+	}
+	sum = (*stack)->next->n + (*stack)->n;
+	_pop(stack, number);
+
+	(*stack)->n = sum;
+}
 
 /**
  * _pchar - prints the ASCII value of a number
