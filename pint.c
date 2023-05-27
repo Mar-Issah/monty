73a04b5cad70c24a1b3ee156b79cab699c02ab12
Mar-Issah <masy370@gmail.com>
2023-05-26 20:36:26 -0400

    implement pint and pop function

--- pint.c
+++ pint.c
@@ -1,20 +1,20 @@
 
 #include "monty.h"
 /**
- * _pint - print int a top of stack
- * @stack: pointer to linked list stack
- * @number: number of line opcode occurs on
+ * _pint -  prints the value at the top of stack
+ * @stack: pointer to the linked list stack
+ * @number: number of line
  *
  */
 void _pint(stack_t **stack, unsigned int number)
 {
-	stack_t *runner;
+	stack_t *h;
 
-	runner = *stack;
-	if (runner == NULL)
+	h = *stack;
+	if (h == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", number);
 		exit(EXIT_FAILURE);
 	}
-	printf("%d\n", runner->n);
+	printf("%d\n", h->n);
 }
--- pop.c
+++ pop.c
@@ -1,21 +1,21 @@
 #include "monty.h"
 /**
- * _pop - remove element a list
+ * _pop - pop an element a list
  *@stack: pointer to first node
- *@number: integer
+ *@number: int
  *Return: void
  */
 void _pop(stack_t **stack, unsigned int number)
 {
-	stack_t *nodo = *stack;
+	stack_t *currentNode = *stack;
 
 	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", number);
 		exit(EXIT_FAILURE);
 	}
-	*stack = nodo->next;
+	*stack = currentNode->next;
 	if (*stack != NULL)
 		(*stack)->prev = NULL;
-	free(nodo);
+	free(currentNode);
 }
