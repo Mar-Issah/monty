73663d8680d94a422b5197d84ba7a6a343800d49
Mar-Issah <masy370@gmail.com>
2023-05-26 22:03:08 -0400

    implement pchar and pstr

--- stack_func2.c
+++ stack_func2.c
@@ -34,39 +34,6 @@ void _add(stack_t **stack, unsigned int number)
 }
 
 /**
- * _pchar - prints the ASCII value of a number
- * @stack: pointer to the top of the stack
- * @number: the index of the current line
- *
- */
-void _pchar(stack_t **stack, unsigned int number)
-{
-	int val;
-
-	if (stack == NULL || *stack == NULL)
-	{
-		fprintf(stderr, "L%d: can't pchar, stack empty\n", number);
-		free(global.buffer);
-		fclose(global.file);
-		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
-
-	val = (*stack)->n;
-	if (val > 127 || val < 0)
-	{
-		fprintf(stderr, "L%d: can't pchar, value out of range\n", number);
-		free(global.buffer);
-		fclose(global.file);
-		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
-
-	putchar(val);
-	putchar('\n');
-}
-
-/**
  * _isalphabet - checks if int is in alphabet
  * @c: int
  * Return: 1 if yes, 0 if no
--- stack_func3.c
+++ stack_func3.c
@@ -1,30 +1,6 @@
 #include "monty.h"
 
 /**
- * _pstr - mod top of stack y second top stack
- * @stack: pointer to lists for monty stack
- * @number: number of line opcode occurs on
- */
-void _pstr(stack_t **stack, unsigned int number)
-{
-	stack_t *temp = *stack;
-	int c = 0;
-
-	(void)number;
-
-
-	while (temp)
-	{
-		c = temp->n;
-		if (c == 0 || _isalphabet(c) == 0)
-			break;
-		putchar(c);
-		temp = temp->next;
-	}
-	putchar('\n');
-}
-
-/**
  * _rotl - mod top of stack y second top stack
  * @stack: pointer to lists for monty stack
  * @number: number of line opcode occurs on
@@ -32,7 +8,6 @@ void _pstr(stack_t **stack, unsigned int number)
 void _rotl(stack_t **stack, unsigned int number)
 {
 	stack_t *runner = *stack;
-
 
 	int aux1 = 0;
 
